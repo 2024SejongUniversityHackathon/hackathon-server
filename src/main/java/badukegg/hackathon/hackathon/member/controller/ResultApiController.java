75cd6a3667894d4788672fc4aad8a824162a7de2
nyeroni <nir2y@naver.com>
2024-05-18 04:57:24 +0900

    feat: 부가 설명 추가

--- src/main/java/badukegg/hackathon/hackathon/member/controller/ResultApiController.java
+++ src/main/java/badukegg/hackathon/hackathon/member/controller/ResultApiController.java
@@ -3,6 +3,9 @@ package badukegg.hackathon.hackathon.member.controller;
 import badukegg.hackathon.hackathon.common.response.ApiResponseCustom;
 import badukegg.hackathon.hackathon.common.response.ResponseCode;
 import badukegg.hackathon.hackathon.member.service.ResultService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,11 @@ public class ResultApiController {
         this.resultService = resultService;
     }
 
+    @Operation(summary = "생활기록부와 설문조사를 모두 완료했는지 검사", description = "생활기록부와 설문조사를 모두 완료했는지 검사하는 Controller")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "모두 성공"),
+            @ApiResponse(responseCode = "500", description = "둘 중 하나를 하지 않았습니다.")
+    })
     @GetMapping("/members/{memberId}/check")
     public ApiResponseCustom<?> checkAvailability(@PathVariable Long memberId) {
         boolean isAvailable = resultService.isMemberValid(memberId);
--- src/main/java/badukegg/hackathon/hackathon/member/controller/ScoreApiController.java
+++ src/main/java/badukegg/hackathon/hackathon/member/controller/ScoreApiController.java
@@ -3,6 +3,10 @@ package badukegg.hackathon.hackathon.member.controller;
 import badukegg.hackathon.hackathon.common.response.ApiResponseCustom;
 import badukegg.hackathon.hackathon.common.response.ResponseCode;
 import badukegg.hackathon.hackathon.member.service.ScoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +24,12 @@ public class ScoreApiController {
     }
 
     @PostMapping("/scores")
-    public ApiResponseCustom<String> saveScores(@RequestBody List<Integer> scores) {
+    @Operation(summary = "점수 입력", description = "점수 입력하는 Controller" )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "점수 입력 성공"),
+            @ApiResponse(responseCode = "500", description = "점수가 정상적으로 입력되지 않았습니다.")
+    })
+    public ApiResponseCustom<String> saveScores(@RequestBody @Schema(example = "[1,2,3,4,5,6]") List<Integer> scores) {
         if (scores.size() != 6) {
             return ApiResponseCustom.fail(ResponseCode.SCORE_ERROR);
         }
